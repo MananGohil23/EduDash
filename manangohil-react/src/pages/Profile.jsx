a1e66d28cc3c77f53eaa116292c625a4aaa1b8d5
Manan Gohil <manangohil2357@gmail.com>
2026-05-05 20:37:46 +0530

    removed unused page content

--- manangohil-react/src/pages/Profile.jsx
+++ manangohil-react/src/pages/Profile.jsx
@@ -2,23 +2,7 @@ import React from 'react'
 const Profile = () => {
     return (
         <>
-        <div className = "bg-gray-200 min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 p-4">
-                    <div className = "rounded-3xl border border-gray-300 bg-white h-screen font-bold flex items-center justify-center shadow">
-                        <div>
-                             This is Blue
-                        </div>
-                    </div>
-                    <div className = "rounded-3xl border border-gray-300 bg-white h-screen font-bold flex items-center justify-center shadow">
-                        <div>
-                            This is Orange
-                        </div>
-                    </div>
-                    <div className = "rounded-3xl border border-gray-300 bg-white h-screen font-bold flex items-center justify-center shadow">
-                        <div>
-                            This is Green
-                        </div>
-                    </div>
-        </div>
+        Work in progress
         </>
     );
 };
