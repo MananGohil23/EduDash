b30a38b1e47c06bf56ed2d60c5d686b7d0f765a5
Manan Gohil <manangohil2357@gmail.com>
2026-05-08 22:21:54 +0530

    added navbar in assignments page

--- manangohil-react/src/pages/learning/Assignments.jsx
+++ manangohil-react/src/pages/learning/Assignments.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 const Assignments = () => {
     return (
         <>
-        Work in progress
+        <div className = "bg-gray-100 h-full flex flex-col justify-center items-center">
+            <div className = "flex items-center justify-center gap-4 bg-white w-full shadow-lg rounded-lg p-2">
+                <button className = "transition text-Slate-700 text-xl px-4 py-3 shadow-lg rounded-lg hover:bg-red-600 hover:scale-[110%] hover:-translate-y-[5px]">Forthcoming</button>
+                <button className = "transition text-Slate-700 text-xl px-4 py-3 shadow-lg rounded-lg hover:bg-red-600 hover:scale-[110%] hover:-translate-y-[5px]">Submitted</button>
+                <button className = "transition text-Slate-700 text-xl px-4 py-3 shadow-lg rounded-lg hover:bg-red-600 hover:scale-[110%] hover:-translate-y-[5px]">Past Due</button>
+
+            </div>
+        </div>
         </>
     );
 };
