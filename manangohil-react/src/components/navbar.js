a4266368cd3b3406c8ccb2ddc4da97b8814efb25
Manan Gohil <manangohil2357@gmail.com>
2026-05-10 23:47:44 +0530

    trying to make a separate file for the Login Page

--- manangohil-react/src/components/navbar.js
+++ manangohil-react/src/components/navbar.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
 import { X } from "lucide-react";
-
-
+import LoginPage from "./LoginPage";
 
 const Navbar = () => {
   const [isloggedin, setIsLoggedIn] = useState(false);
@@ -16,93 +15,113 @@ const Navbar = () => {
       setIsLoggedIn(true);
       setLoginCardActive(false);
       alert("Login successful!");
-    }
-    else{
+    } else {
       setIsLoggedIn(false);
       setIncorrectLogin(true);
       alert("Incorrect username or password. Please try again.");
     }
   };
-
   return (
-      <>
-        <nav className=" w-full bg-slate-900 text-white px-4 py-3 shadow-lg shadow-slate-950/30 z-50">
-          <div className="flex justify-between items-center gap-3">
-            <Link
-              to="/"
-              className="text-lg font-semibold tracking-wide"
-              title="Student Dashboard"
+    <>
+      <nav className=" w-full bg-slate-900 text-white px-4 py-3 shadow-lg shadow-slate-950/30 z-50">
+        <div className="flex justify-between items-center gap-3">
+          <Link
+            to="/"
+            className="text-lg font-semibold tracking-wide"
+            title="Student Dashboard"
+          >
+            <div className="flex items-center gap-2 transition hover:scale-[110%] hover:-translate-y-[5px] transition-delay-100">
+              <img
+                src="/logo.png"
+                alt="Logo"
+                className="h-8 w-8 inline-block mr-2"
+              />
+              <div>EduDash</div>
+            </div>
+          </Link>
+          <div className="flex gap-2">
+            <NavLink
+              className={({ isActive }) =>
+                `rounded-full bg-slate-900 px-4 py-2 text-lg font-bold transition ${
+                  isActive
+                    ? "border-white bg-white text-slate-900"
+                    : "border-white/25 text-white hover:border-white hover:bg-blue-600 hover:scale-[110%] hover:-translate-y-[5px] "
+                }`
+              }
+              to="/attendance"
             >
-              <div className="flex items-center gap-2 transition hover:scale-[110%] hover:-translate-y-[5px] transition-delay-100">
-                <img
-                  src="/logo.png"
-                  alt="Logo"
-                  className="h-8 w-8 inline-block mr-2"
-                />
-                <div>EduDash</div>
-              </div>
-            </Link>
-            <div className="flex gap-2">
-              <NavLink
-                className={({ isActive }) =>
-                  `rounded-full bg-slate-900 px-4 py-2 text-lg font-bold transition ${
-                    isActive
-                      ? "border-white bg-white text-slate-900"
-                      : "border-white/25 text-white hover:border-white hover:bg-blue-600 hover:scale-[110%] hover:-translate-y-[5px] "
-                  }`
-                }
-                to="/attendance"
+              Attendance
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `rounded-full bg-slate-900 px-4 py-2 text-lg font-bold transition ${
+                  isActive
+                    ? "border-white bg-white text-slate-900"
+                    : "border-white/25 text-white hover:border-white hover:bg-blue-600 hover:scale-[110%] hover:-translate-y-[5px] "
+                }`
+              }
+              to="/learning/Lectures"
+            >
+              Lectures
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `rounded-full bg-slate-900 px-4 py-2 text-lg font-bold transition ${
+                  isActive
+                    ? "border-white bg-white text-slate-900"
+                    : "border-white/25 text-white hover:border-white hover:bg-blue-600 hover:scale-[110%] hover:-translate-y-[5px] "
+                }`
+              }
+              to="/learning/Assignments"
+            >
+              Assignments
+            </NavLink>
+          </div>
+          <div
+            className={`flex items-center gap-2 ${isloggedin ? "hidden" : "block"}`}
+          >
+            <button
+              className="rounded-full bg-emerald-500 px-4 py-2 text-lg hover:scale-[110%] font-semibold text-white transition hover:bg-emerald-400 hover:-translate-y-[5px]"
+              onClick={() => {
+                setLoginCardActive(true);
+                setIncorrectLogin(false);
+                document.getElementById("passwordInput").value = "";
+                document.getElementById("usernameInput").value = "";
+              }}
+            >
+              Login
+            </button>
+          </div>
+          <div
+            className={`flex items-center gap-2 ${isloggedin ? "block" : "hidden"}`}
+          >
+            <div className="flex items-center gap-2">
+              <Link
+                to="/"
+                onClick={() => {
+                  setIsLoggedIn(false);
+                  alert("You have been logged out.");
+                  document.getElementById("passwordInput").value = "";
+                  document.getElementById("usernameInput").value = "";
+                }}
+                className="rounded-full bg-emerald-500 px-4 py-2 text-lg hover:scale-[110%] font-semibold text-white transition hover:bg-emerald-400 hover:-translate-y-[5px]"
               >
-                Attendance
-              </NavLink>
-              <NavLink
-                className={({ isActive }) =>
-                  `rounded-full bg-slate-900 px-4 py-2 text-lg font-bold transition ${
-                    isActive
-                      ? "border-white bg-white text-slate-900"
-                      : "border-white/25 text-white hover:border-white hover:bg-blue-600 hover:scale-[110%] hover:-translate-y-[5px] "
-                  }`
-                }
-                to="/learning/Lectures"
-              >
-                Lectures
-              </NavLink>
-              <NavLink
-                className={({ isActive }) =>
-                  `rounded-full bg-slate-900 px-4 py-2 text-lg font-bold transition ${
-                    isActive
-                      ? "border-white bg-white text-slate-900"
-                      : "border-white/25 text-white hover:border-white hover:bg-blue-600 hover:scale-[110%] hover:-translate-y-[5px] "
-                  }`
-                }
-                to="/learning/Assignments"
-              >
-                Assignments
-              </NavLink>
+                Logout
+              </Link>
             </div>
-            <div className = {`flex items-center gap-2 ${(isloggedin ? 'hidden' : 'block')}`}>
-                <button className = "rounded-full bg-emerald-500 px-4 py-2 text-lg hover:scale-[110%] font-semibold text-white transition hover:bg-emerald-400 hover:-translate-y-[5px]" onClick={() => {setLoginCardActive(true); setIncorrectLogin(false); document.getElementById("passwordInput").value = ""; document.getElementById("usernameInput").value = ""}}>
-                  Login
-                </button>
-            </div>
-            <div className = {`flex items-center gap-2 ${(isloggedin ? 'block' : 'hidden')}`}>
-              <div className = "flex items-center gap-2">
-                <Link to="/" onClick={() => {setIsLoggedIn(false); alert("You have been logged out."); document.getElementById("passwordInput").value = ""; document.getElementById("usernameInput").value = ""}} className="rounded-full bg-emerald-500 px-4 py-2 text-lg hover:scale-[110%] font-semibold text-white transition hover:bg-emerald-400 hover:-translate-y-[5px]">
-                      Logout
-                </Link>
-              </div>
-              <div className = "flex items-center gap-2">
-                <Link
-                  to="/profile"
-                  className="rounded-full bg-emerald-500 px-4 py-2 text-lg hover:scale-[110%] font-semibold text-white transition hover:bg-emerald-400 hover:-translate-y-[5px]"
-                >
-                  Profile
-                </Link>
-              </div>
+            <div className="flex items-center gap-2">
+              <Link
+                to="/profile"
+                className="rounded-full bg-emerald-500 px-4 py-2 text-lg hover:scale-[110%] font-semibold text-white transition hover:bg-emerald-400 hover:-translate-y-[5px]"
+              >
+                Profile
+              </Link>
             </div>
           </div>
-        </nav>
-        <div className = {`fixed inset-0 bg-white backdrop-blur-sm bg-opacity-50 flex items-center justify-center transition-opacity duration-300 z-50 ${loginCardActive ? 'block' : 'hidden'}`} >
+        </div>
+      </nav>
+      {/* <LoginPage /> */}
+      <div className = {`fixed inset-0 bg-white backdrop-blur-sm bg-opacity-50 flex items-center justify-center transition-opacity duration-300 z-50 ${loginCardActive ? 'block' : 'hidden'}`} >
           <div className = {`bg-white rounded-lg shadow-lg relative p-6 w-80 text-center`}>
               <div className = "flex justify-center gap-4"> 
                 <h1 className = "text-2xl text-slate-900 mb-6 font-bold">
@@ -121,9 +140,9 @@ const Navbar = () => {
                 Login
               </button>
           </div>
-        </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
