96b123d7ad19094ae3745986e948eff8d3312c0f
Manan Gohil <manangohil2357@gmail.com>
2026-06-06 21:14:58 +0530

    Update API URLs for production

--- frontend/src/services/assignmentService.js
+++ frontend/src/services/assignmentService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API =
-"http://localhost:5000/api/assignments";
+"https://edudash-hyka.onrender.com/api/assignments";
 
 const getToken = () =>
     sessionStorage.getItem("token");
